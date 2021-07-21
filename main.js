850f2f413922f22a86c20b54ab540c7b7b1e1e45
Michael <mike.karpinski99@gmail.com>
2021-07-21 16:03:07 -0400

    created Store class to manage data.

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const os = require('os')
+const Store = require('./store.js')
+
+const store = new Store({
+    configName: 'user-data',
+    defaults: {
+        windowBounds: { width: 800, height: 600 }
+    }
+})
 
 function createWindow() {
+    let { width, height } = store.get('windowBounds')
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: width,
+        height: height,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
 
-    win.loadFile('index.html')
+    win.loadFile('front_end/index.html')
+    return win
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    const window = createWindow()
 
     app.on('activate', function () {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+
+    window.on('resize', () => {
+        let { width, height } = window.getBounds()
+        store.set('windowBounds', { width, height })
+        console.log('WINDOW RESIZED')
     })
 })
 
--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
     // Allows access to both renderer globals and Node.js environment.
-    document.getElementById('preload-text').innerText = 'Budget Manager'
+    document.getElementById('income').innerText = '~$2,000'
 })
