e60e72e59ee49e7bcdb34377c5030ac8c061dcfd
Michael <mike.karpinski99@gmail.com>
2021-07-21 15:23:08 -0400

    initialized Electron app.

---
+++ preload.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Allows access to both renderer globals and Node.js environment.
+    document.getElementById('preload-text').innerText = 'Budget Manager'
+})
