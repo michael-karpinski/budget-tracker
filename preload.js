092d7d0941fadce87c425dff94c92278af0b3b25
Michael <mike.karpinski99@gmail.com>
2021-07-22 11:56:55 -0400

    encapsulated Income functionality.

--- preload.js
+++ preload.js
@@ -1,12 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron')
+const Income = require('./income.js')
 
 window.addEventListener('DOMContentLoaded', () => {
     // Allows access to both renderer globals and Node.js environment.
-    const addIncomeButton = document.getElementById('add-income-button')
-    const addIncomeForm = document.getElementById('add-income-form')
-    const newIncomeSource = document.getElementById('new-income-source')
-    const newIncomeAmount = document.getElementById('new-income-amount')
-    const incomeDiv = document.getElementById('income-div')
 
     const addExpenseButton = document.getElementById('add-expense-button')
     const addExpenseForm = document.getElementById('add-expense-form')
@@ -17,15 +13,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const newExpenseDate = document.getElementById('new-expense-date')
     const expenseDiv = document.getElementById('expense-div')
 
-    let incomeData
     let expenseData
 
-    // Setup communication for income data.
-    ipcRenderer.send('get', 'income')
-    ipcRenderer.on('incomeReply', (event, arg) => {
-        incomeData = arg
-        populateIncome(incomeData, incomeDiv)
-    })
+    let income = new Income(document.getElementById('income-div'))
 
     // Setup communication for expense data.
     ipcRenderer.send('get', 'expense')
@@ -34,34 +24,8 @@ window.addEventListener('DOMContentLoaded', () => {
         populateExpense(expenseData, expenseDiv)
     })
 
-    ipcRenderer.on('incomeSaved', (event, arg) => {
-        populateIncome(incomeData, incomeDiv)
-    })
-
     ipcRenderer.on('expenseSaved', (event, arg) => {
         populateExpense(expenseData, expenseDiv)
-    })
-
-    // When "Add Income" button is clicked, show the add income form.
-    addIncomeButton.addEventListener('click', () => {
-        addIncomeForm.removeAttribute('hidden')
-    })
-
-    // When add income form is submitted, save new income data, reset and hide form, and populate income display.
-    addIncomeForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-
-        incomeData.push({
-            'src': newIncomeSource.value,
-            'income': parseFloat(newIncomeAmount.value).toFixed(2)
-        })
-        saveData('income', incomeData)
-
-        addIncomeForm.setAttribute('hidden', '')
-        newIncomeAmount.value = ''
-        newIncomeSource.value = ''
-
-        populateIncome(incomeData, incomeDiv)
     })
 
     // When "Add Expense" button is clicked, show the add expense form.
@@ -101,19 +65,6 @@ window.addEventListener('DOMContentLoaded', () => {
             newExpenseDateVisibility.setAttribute('hidden', '')
     })
 })
-
-function populateIncome(incomeData, incomeDiv) {
-    // Populate income display.
-    incomeDiv.innerHTML = ''
-    incomeData.forEach((incomeSource => {
-        const header = document.createElement('h2')
-        header.innerText = incomeSource.src
-        const amount = document.createElement('p')
-        amount.innerText = incomeSource.income
-        incomeDiv.appendChild(header)
-        incomeDiv.appendChild(amount)
-    }))
-}
 
 function populateExpense(expenseData, expenseDiv) {
     // Populate expense display.
